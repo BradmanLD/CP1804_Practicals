25d5f123e01a3afc72ddf66a69066a90c7fd0951
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-03-13 08:11:51 +1000

    Add functions to ascii table

--- Prac02/asciiTable.py
+++ Prac02/asciiTable.py
@@ -1,10 +1,24 @@
 LOWER_BOUND = 33
 UPPER_BOUND = 127
-character = str(input("Enter a character: "))
-print("The ASCII code for {} is {}".format(character, ord(character)))
-number = int(input("Enter a number between {} and {}: ".format(LOWER_BOUND, UPPER_BOUND)))
-while number < LOWER_BOUND or number > UPPER_BOUND:
-    number = int(input("Invalid number, Enter a number between {} and {}: ".format(LOWER_BOUND, UPPER_BOUND)))
-print("The character for {} is {}".format(number, chr(number)))
-for i in range(LOWER_BOUND, UPPER_BOUND):
-    print("{0} {1:>6}".format(i, chr(i)))
+
+
+def main():
+    character = str(input("Enter a character: "))
+    print("The ASCII code for {} is {}".format(character, ord(character)))
+    ascii_number = get_number(LOWER_BOUND, UPPER_BOUND)
+    print("The character for {} is {}".format(ascii_number, chr(ascii_number)))
+    for i in range(LOWER_BOUND, UPPER_BOUND):
+        print("{0} {1:>6}".format(i, chr(i)))
+
+
+def get_number(lower, upper):
+    number = 0
+    while number < lower or number > upper:
+        try:
+            number = int(input("Enter a number between {} and {}: ".format(lower, upper)))
+        except ValueError:
+            print("Invalid Input: Try again.")
+    return number
+
+
+main()
