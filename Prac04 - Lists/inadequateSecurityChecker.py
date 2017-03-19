a07fc2e6e3137cd0e4e501cd6fe0bd57c545f34e
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-03-20 06:54:23 +1000

    Add inadequateSecurityChecker

---
+++ Prac04 - Lists/inadequateSecurityChecker.py
@@ -0,0 +1,16 @@
+"""
+Ask the user for their username. If the username is in the above list of authorised users, print “Access
+granted”, otherwise print “Access denied”.
+"""
+
+def main():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
+                 'swei45''BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+                 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+    username = input("Enter username: ")
+    if username in usernames:
+        print("Access Granted")
+    else:
+        print("Access Denied")
+
+main()
