6457ae3a4e8f281f089e2aa1d44823c118ab0dc4
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-03-20 10:43:01 +1000

    Separate parts of Security Checker to make it more readable

--- Prac04 - Lists/inadequateSecurityChecker.py
+++ Prac04 - Lists/inadequateSecurityChecker.py
@@ -9,6 +9,7 @@ def main():
                  'swei45''BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
                  'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
     username = input("Enter username: ")
+
     if username in usernames:
         print("Access Granted")
     else:
