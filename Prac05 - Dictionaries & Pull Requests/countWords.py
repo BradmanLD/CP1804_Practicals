86d2c55c46aeaa67fdc9f9d4e4ecb7aa5adad906
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-04-03 07:07:56 +1000

    Add formatting to countWords

--- Prac05 - Dictionaries & Pull Requests/countWords.py
+++ Prac05 - Dictionaries & Pull Requests/countWords.py
@@ -6,13 +6,12 @@ then print the counts of how many of each word are in the file.
 
 def main():
     dictionary = {}
-    # words = input("Enter a sentence: ").split()
-    words = "this is a sentence".split()
+    words = input("Enter a sentence: ").split()
     for word in words:
         if word in dictionary:
             dictionary[word] += 1
         else:
             dictionary[word] = 1
     for word in sorted(dictionary):
-        print("{} : {}".format(word, dictionary[word]))
+        print("{:{}} : {:2}".format(word, len(max(words, key=len)), dictionary[word]))
 main()
