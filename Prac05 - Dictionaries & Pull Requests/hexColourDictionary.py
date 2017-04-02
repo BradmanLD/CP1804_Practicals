4a1e6cb37762a6de3ac3ae4e5d55d95831634529
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-04-03 06:32:14 +1000

    add hexColourDictionary

---
+++ Prac05 - Dictionaries & Pull Requests/hexColourDictionary.py
@@ -0,0 +1,7 @@
+HEX_COLOURS = {"AliceBlue": "#f0f8ff", "Black": "#000000", "LightBlue": "#add8e6", "Magenta": "#ff00ff",
+               "Violet" : "#ee82ee", "White": "#ffffff", "Aquamarine1": "#7fffd4", "Azure1": "#f0ffff",
+               "Beige": "#f5f5dc", "BurlyWood": "#deb887"}
+
+
+hex_colour = input("Enter the name of a colour: ")
+print(HEX_COLOURS[hex_colour])
