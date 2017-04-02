92734644e1f4851cd3f26da02fb99b1c7dc8fbf4
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-04-03 06:53:52 +1000

    Add countWords

---
+++ Prac05 - Dictionaries & Pull Requests/countWords.py
@@ -0,0 +1,18 @@
+"""
+Write a program to count the occurrences of words in a string. The program should ask the user for a string,
+then print the counts of how many of each word are in the file.
+"""
+
+
+def main():
+    dictionary = {}
+    # words = input("Enter a sentence: ").split()
+    words = "this is a sentence".split()
+    for word in words:
+        if word in dictionary:
+            dictionary[word] += 1
+        else:
+            dictionary[word] = 1
+    for word in sorted(dictionary):
+        print("{} : {}".format(word, dictionary[word]))
+main()
--- Prac05 - Dictionaries & Pull Requests/hexColourDictionary.py
+++ Prac05 - Dictionaries & Pull Requests/hexColourDictionary.py
@@ -1,3 +1,9 @@
+"""
+Use a constant dictionary of about 10 names and write a program that allows a user to enter a name and get
+the code, e.g. entering AliceBlue should show #f0f8ff.
+"""
+
+
 HEX_COLOURS = {"AliceBlue": "#f0f8ff", "Black": "#000000", "LightBlue": "#add8e6", "Magenta": "#ff00ff",
                "Violet" : "#ee82ee", "White": "#ffffff", "Aquamarine1": "#7fffd4", "Azure1": "#f0ffff",
                "Beige": "#f5f5dc", "BurlyWood": "#deb887"}
