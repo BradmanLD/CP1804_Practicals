6089ee5a3177cf6c395e4344c881e422c7428dec
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-04-10 09:04:16 +1000

    Add basic kivy structure

---
+++ Prac06 - Kivy/convert_miles_to_km.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.app import Builder
+
+
+class ConvertMilesToKilometresApp(App):
+    def build(self):
+        self.title = 'Convert Miles to Kilometres'
+        self.root = Builder.load_file('convert_miles_to_km.kv')
+        return self.root()
+
