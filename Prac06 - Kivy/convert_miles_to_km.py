4ee5303e5f7d7a3085c9c1c5be59b46b951c0e27
BradmaLD <bradman.davis@my.jcu.edu.au>
2017-04-10 10:38:54 +1000

    Add functionality to buttons, Add conversion calculation

--- Prac06 - Kivy/convert_miles_to_km.py
+++ Prac06 - Kivy/convert_miles_to_km.py
@@ -1,12 +1,31 @@
 from kivy.app import App
 from kivy.app import Builder
 
+MILES_TO_KM = 1.60934
+
 
 class ConvertMilesToKilometresApp(App):
     def build(self):
         self.title = 'Convert Miles to Kilometres'
         self.root = Builder.load_file('convert_miles_to_km.kv')
         return self.root
+
+    def handle_increment(self, increment):
+        incremented_number = self.get_miles() + increment
+        self.root.ids.number_of_miles.text = str(incremented_number)
+
+    def get_miles(self):
+        try:
+            miles = float(self.root.ids.number_of_miles.text)
+            return miles
+
+        except ValueError:
+            return 0
+
+    def calculate_miles_to_kilometres(self):
+        number_of_miles = self.get_miles()
+        number_of_kilometres = number_of_miles * MILES_TO_KM
+        self.root.ids.output_label.text = str(number_of_kilometres) + " kilometres"
 
 
 ConvertMilesToKilometresApp().run()
