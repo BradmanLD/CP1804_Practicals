f2361118a2a278d4f4402c7a60315e200d34eef2
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-04-21 08:50:11 +1000

    Add getting input

--- Prac07/guitarTest.py
+++ Prac07/guitarTest.py
@@ -2,18 +2,36 @@ from Prac07.guitar import Guitar
 
 
 def main():
-    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
-    test_guitar_one = Guitar("Name", 1950, 2004.23)
-    test_guitar_two = Guitar("TestName", 2004, 200.3003)
+    print("My guitars!")
+    guitars = []
+    while True:
+        name = input("Name: ")
+        if name.strip() == "":
+            break
+        year = int(input("Year: "))
+        cost = int(input("Cost: $"))
+        guitars.append(Guitar(name, year, cost))
 
-    guitars = [gibson, test_guitar_one, test_guitar_two]
-    for guitar in guitars:
+    # gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    # test_guitar_one = Guitar("Name", 1950, 2004.23)
+    # test_guitar_two = Guitar("TestName", 2004, 200.3003)
+
+    for i, guitar in enumerate(guitars):
         if guitar.is_vintage():
-            print(guitar.name + " is vintage")
+            vintage_string = "(vintage)"
         else:
-            print(guitar.name + " is not vintage")
+            vintage_string = ""
 
-    print(gibson)
+        print("Guitar {}: {:>20} ({}), worth ${:10,.2f} {}"
+              .format(i + 1, guitar.name, guitar.year, guitar.cost, vintage_string))
+
+    # for guitar in guitars:
+    #     if guitar.is_vintage():
+    #         print(guitar.name + " is vintage")
+    #     else:
+    #         print(guitar.name + " is not vintage")
+
+    # print(gibson)
 
 
 main()
