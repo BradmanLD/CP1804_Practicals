8ff97ec0ff16873a6168c1141ba6fbb40dcfb93c
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-04-21 06:54:13 +1000

    Add __init__

---
+++ Prac07/programmingLanguage.py
@@ -0,0 +1,8 @@
+
+
+class ProgrammingLanguage:
+    def __init__(self, name, typing_type, can_reflect, year):
+        self.name = name
+        self.typing_type = typing_type
+        self.can_reflect = can_reflect
+        self.year = year
