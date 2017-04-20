6ad1f4f17ccb209cd236219a0620fdfa897d8b6c
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-04-21 06:31:20 +1000

    Add basic limo test

--- Prac07/usedCars.py
+++ Prac07/usedCars.py
@@ -6,14 +6,20 @@ from Prac07.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    bus = Car(180)
-    bus.drive(30)
-    print("fuel =", bus.fuel)
-    print("odo =", bus.odometer)
-    print(bus)
+    # bus = Car(180)
+    # bus.drive(30)
+    # print("fuel =", bus.fuel)
+    # print("odo =", bus.odometer)
+    # print(bus)
+    #
+    # print("Car {}, {}".format(bus.fuel, bus.odometer))
+    # print("Car {self.fuel}, {self.odometer}".format(self=bus))
 
-    print("Car {}, {}".format(bus.fuel, bus.odometer))
-    print("Car {self.fuel}, {self.odometer}".format(self=bus))
+    limo = Car(100)
+    limo.add_fuel(20)
+    print("fuel =", limo.fuel)
+    limo.drive(115)
+    print("odo =", limo.odometer)
 
 
 main()
