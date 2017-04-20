840a427a126dd69d0242930e3fee7dc975bf6474
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-04-21 07:01:38 +1000

    Test ProgrammingLanguages class

---
+++ Prac07/languages.py
@@ -0,0 +1,11 @@
+from Prac07.programmingLanguage import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    vb = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(ruby)
+
+
+main()
