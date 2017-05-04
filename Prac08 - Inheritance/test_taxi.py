7a697e06e120a59c83098a706a0a11c9998609f2
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-05-05 07:34:12 +1000

    Add testing for Taxi

---
+++ Prac08 - Inheritance/test_taxi.py
@@ -0,0 +1,13 @@
+from taxi import Taxi
+
+# Test creating a new taxi
+prius_one = Taxi("Prius 1", 100, 1.2)
+
+# Test driving & __str__
+prius_one.drive(40)
+print(prius_one)
+
+# Test resetting current fare
+prius_one.start_fare()
+prius_one.drive(100)
+print(prius_one)
