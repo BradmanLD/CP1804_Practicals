33b78c2a53c552005c6bdc2474ab86f89fdea139
BradmanLD <bradman.davis@my.jcu.edu.au>
2017-05-05 07:47:58 +1000

    Test UnreliableCar

---
+++ Prac08 - Inheritance/test_unreliableCar.py
@@ -0,0 +1,9 @@
+from unreliableCar import UnreliableCar
+
+# Test creating unreliable car
+terrible_car = UnreliableCar("Terrible", 100, 5)
+print(terrible_car)
+
+# Test reliability
+terrible_car.drive(50)
+print(terrible_car)
--- Prac08 - Inheritance/unreliableCar.py
+++ Prac08 - Inheritance/unreliableCar.py
@@ -1,1 +1,18 @@
 from car import Car
+from random import randint
+
+
+class UnreliableCar(Car):
+    def __init__(self, name, fuel, reliability=0.0):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        if self.reliability > randint(0, 100):
+            if distance > self.fuel:
+                distance = self.fuel
+                self.fuel = 0
+            else:
+                self.fuel -= distance
+            self.odometer += distance
+        return distance
